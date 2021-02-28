0509e77996c24eec39c5df26c49ac9221c312331
Annie Liao <liaowow@gmail.com>
2021-02-27 19:37:31 -0500

    build app layout components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,7 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-        <h2>Enter a Store</h2>
+        <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store →</button>
       </form>
