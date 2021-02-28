c0bbcfe9c1dc171923206a09a13ece8661f9f082
Annie Liao <liaowow@gmail.com>
2021-02-28 11:54:31 -0500

    build order component to update and display order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     fishes: {},
     order: {},
   };
+
   addFish = fish => {
     // take a copy of existing state
     const fishes = {...this.state.fishes};
@@ -17,21 +18,41 @@ class App extends React.Component {
     // set the new fishes obj to state
     this.setState({ fishes });
   }
+
   loadSampleFishes = () => {
     this.setState({
       fishes: sampleFishes
     })
   }
+
+  addToOrder = (key) => {
+    // take a copy of existing state
+    const order = {...this.state.order};
+    // add to the order, or update number of order
+    order[key] = order[key] + 1 || 1;
+    // set the state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="I Am Cool" age={999} />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} 
+                    index={key}
+                    details={this.state.fishes[key]}
+                    addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order}
+        />
         <Inventory 
           addFish={this.addFish} 
           loadSampleFishes={this.loadSampleFishes} 
