c7ba8d06c4ca312097c6e59d340602b67274f7b1
Annie Liao <liaowow@gmail.com>
2021-02-28 11:13:41 -0500

    create fish form and display sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = fish => {
+    // take a copy of existing state
+    const fishes = {...this.state.fishes};
+    // add new fish to fishes
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes obj to state
+    this.setState({ fishes });
+  }
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="I Am Cool" age={999} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory 
+          addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes} 
+        />
       </div>
     )
   }
